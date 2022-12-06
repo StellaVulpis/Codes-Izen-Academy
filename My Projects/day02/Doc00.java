4cdf196c1594ba5ba944a3b09029387cf5507b6b
Jordan <84948603+StellaVulpis@users.noreply.github.com>
2022-12-06 17:11:38 +0900

    Add files via upload

---
+++ My Projects/day02/Doc00.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class Doc00 
+{
+	public static void main(String args[])
+	{
+		// int a;
+		// a=a+1;  >> Exception occured.
+		// Reason is because you didn't initialize it, so it's value is 'NULL'. 
+		// You can't use variables with null value  to variables. So you must initialize it.
+	}
+}
