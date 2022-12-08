687313367cf01c3f041ce89610066a1ca2998c11
Jordan <84948603+StellaVulpis@users.noreply.github.com>
2022-12-08 17:59:54 +0900

    Add files via upload

---
+++ My Projects/Subject_PyramidMaking/Pyramid.java
@@ -0,0 +1,33 @@
+package Subject_PyramidMaking;
+
+public class Pyramid 
+{
+	public static void main(String[] args) 
+	{
+		MyCls Py = new MyCls();
+		Py.PyramidMaker();
+	}
+}
+class MyCls
+{
+	MyCls()
+	{
+	}
+	void PyramidMaker()
+	{
+		// Algorithem: 5 - 3 - 1 | 1 + 3 + 5
+		for(int i=1;i<=3;i++)
+		{
+			for(int a =1; a<=i; a++)
+			{
+				// Working on it
+				
+			}
+			for(int b =1; b<=i; b++)
+			{
+				// Working on it
+			}
+		System.out.println();
+		}
+	}
+}
