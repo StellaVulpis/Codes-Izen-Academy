c7bcc7e1b6f83026d4687abcd67f56c29f98bf7e
Jordan <84948603+StellaVulpis@users.noreply.github.com>
2022-12-19 21:31:08 +0900

    Update ArrayList_Scanner.java

--- My Projects/day11/ArrayList_Scanner.java
+++ My Projects/day11/ArrayList_Scanner.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class ArrayList_Scanner 
 {
-	
 	Scanner sc = new Scanner(System.in);
-	Scanner tc = new Scanner(System.in);
-	
+	Scanner ac = new Scanner(System.in);
+	public String scanfAns(String text)
+	{
+	    System.out.print(text+": ");
+	    return ac.nextLine();
+	}
 	public String scanfStr(String text)
 	{
-		System.out.print(text);
-		System.out.print(": ");
+		System.out.print(text+": ");
 		return sc.nextLine();
 	}
 	public int scanfInt(String text)
 	{
-		System.out.print(text);
-		System.out.print(": ");
+		System.out.print(text+": ");
 		return sc.nextInt();
 	}
-	
 }
